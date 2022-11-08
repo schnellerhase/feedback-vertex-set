7bb92298c8d9efef246cf54aa0b152b71620a58d
hasi-2 <xDNdnQoyTcrrMdWCVZft4YLqkaRWnRb@protonmail.com>
2022-11-08 15:59:33 +0100

    tools removed

--- tools/analysis/run.sh
+++
@@ -1,8 +0,0 @@
-NP=55
-TIMEOUT=1800 # 30 minutes
-
-mkdir -p data
-for SOLVER in analysis; do
-
-    seq -f "%03g" 1 2 009 | xargs --max-procs=$NP -I [] sh -c "{ /usr/bin/time -f \"%e\" timeout --preserve-status $TIMEOUT ../../build/tools/analysis/$SOLVER ../../tracks/e_[] > /dev/null; }"
-done

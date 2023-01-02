7d1350aafa06045d20ad434e38ca1dbb44350094
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2023-01-02 18:25:33 +0100

    Benchmark CI
    Benchmarking support finalized and added to CI. This might need some more fine tuning to be a reliable indicator though.

---
+++ benchmark/solvers.cpp
@@ -0,0 +1,36 @@
+#include <benchmark/benchmark.h>
+#include <iomanip>
+#include <iostream>
+
+#include "fvs/discrete/discrete.hpp"
+#include "solvers/reduce.hpp"
+#include "solvers/naive.hpp"
+
+auto read_graph(index_t graph_no)
+{
+    std::stringstream ss;
+    ss << std::setfill('0') << std::setw(3) << graph_no;
+    return Graph::read("tracks/e_" + ss.str());
+}
+
+static void
+BM_naive(benchmark::State& state)
+{  
+    Graph graph = read_graph(state.range(0));
+    for (auto _ : state)
+        solve_naive(graph);
+}
+BENCHMARK(BM_naive)->DenseRange(1, 26, 2);
+
+
+static void
+BM_reduce(benchmark::State& state)
+{
+    SubGraph graph = SubGraph(read_graph(state.range(0)));
+    for (auto _ : state)
+        solve_reduce(graph);
+}
+
+// BENCHMARK(BM_reduce)->DenseRange(1, 50, 2);
+BENCHMARK(BM_reduce)->DenseRange(1, 36, 2);
+BENCHMARK_MAIN();
